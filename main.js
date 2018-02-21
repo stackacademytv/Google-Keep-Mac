f4cc23152b52dcb2b919e2edc2c552230cc19320
Ray Viljoen <ray@fubra.com>
2018-02-21 16:54:22 +0200

    Fixes window recreation on activate. v1.2.2

--- main.js
+++ main.js
@@ -8,42 +8,52 @@ const fs = require('fs')
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
+// Create the main BrowserWindow
+const createWindow = () => {
+
+  // Add main menu to app
+  appMenu.create()
+
+  // Load the previous state with fallback to defaults
+  let winState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800
+  })
+
+  // Create the window using the state information
+  win = new BrowserWindow({
+    x: winState.x,
+    y: winState.y,
+    width: winState.width,
+    height: winState.height,
+    backgroundColor: '#E8E8E8',
+    title: 'Google Keep'
+  })
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+
+  // and load the index.html of the app.
+  win.loadURL(`file://${__dirname}/index.html`)
+
+  // Let us register listeners on the window, so we can update the state
+  // automatically (the listeners will be removed when the window is closed)
+  // and restore the maximized or full screen state
+  winState.manage(win)
+
+  // Emitted when the window is closed.
+  win.on('closed', () => win = null )
+
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', () => {
+app.on('ready', createWindow)
 
-    // Add main menu to app
-    appMenu.create()
-
-    // Load the previous state with fallback to defaults
-    let winState = windowStateKeeper({
-      defaultWidth: 1000,
-      defaultHeight: 800
-    })
-
-    // Create the window using the state information
-    win = new BrowserWindow({
-      x: winState.x,
-      y: winState.y,
-      width: winState.width,
-      height: winState.height,
-      backgroundColor: '#E8E8E8',
-      title: 'Google Keep'
-    })
-
-    // Open the DevTools.
-    // win.webContents.openDevTools()
-
-    // and load the index.html of the app.
-    win.loadURL(`file://${__dirname}/index.html`)
-
-    // Let us register listeners on the window, so we can update the state
-    // automatically (the listeners will be removed when the window is closed)
-    // and restore the maximized or full screen state
-    winState.manage(win)
-
-    // Emitted when the window is closed.
-    win.on('closed', () => win = null )
+// On OS X re-create a window when the dock icon is clicked
+// and there are no other windows open.
+app.on('activate', () => {
+  if (win === null) createWindow()
 })
 
 // Open url externally if not "keep.google.com"
@@ -71,11 +81,4 @@ app.on('window-all-closed', () => {
 
   // On OS X stay active until the user quits explicitly
   if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('activate', () => {
-
-  // On OS X re-create a window when the dock icon is clicked
-  // and there are no other windows open.
-  if (win === null) createWindow()
 })
